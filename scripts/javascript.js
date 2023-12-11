cbaee2d2aa5b0c3d91898594c449d91c551de1ee
eliana88 <81966208+elianaCern@users.noreply.github.com>
2023-12-11 10:49:06 +0200

    added according (#2)
    * rebased changes
    
    * edited containers classes
    
    * added portfolio container
    
    * added contacts container
    
    * edited js

---
+++ scripts/javascript.js
@@ -0,0 +1,8 @@
+function toggleAccordion(item) {
+  var arrow = document.getElementById("arrow" + item);
+  if (arrow.classList.contains("bi-chevron-down")) {
+    arrow.classList.replace("bi-chevron-down", "bi-chevron-up");
+  } else {
+    arrow.classList.replace("bi-chevron-up", "bi-chevron-down");
+  }
+}
